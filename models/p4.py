b87b2aedd9b9fe20b15008fc2c175d44cdccd21c
Tom Berry <tberry860@gmail.com>
2021-11-18 18:25:52 -0500

    rename self._weight to self.weight

--- models/p4.py
+++ models/p4.py
@@ -18,18 +18,18 @@ class ConvZ2ToP4(nn.Module):
         self.stride = stride
         self.padding = padding
 
-        self._weight = nn.Parameter(torch.empty(out_channels, in_channels, kernel_size, kernel_size).to(device))
-        nn.init.kaiming_uniform_(self._weight, a=5 ** 0.5)
+        self.weight = nn.Parameter(torch.empty(out_channels, in_channels, kernel_size, kernel_size).to(device))
+        nn.init.kaiming_uniform_(self.weight, a=5 ** 0.5)
         self.bias = nn.Parameter(torch.zeros(out_channels).to(device)) if bias else None
 
     # inp: n x c_in x h x w
     def forward(self, inp):
         n = inp.shape[0]
         # make four copies of the filters, rotated element-wise
-        weight = torch.stack([self._weight.rot90(k, dims=(2, 3)) for k in range(4)], dim=1)
+        weight = torch.stack([self.weight.rot90(k, dims=(2, 3)) for k in range(4)], dim=1)
         # fold per-orientation filters into channel dims for convolution
-        _weight = weight.view(self.out_channels * 4, self.in_channels, self.kernel_size, self.kernel_size)
-        out = F.conv2d(inp, _weight, stride=self.stride, padding=self.padding)
+        weight = weight.view(self.out_channels * 4, self.in_channels, self.kernel_size, self.kernel_size)
+        out = F.conv2d(inp, weight, stride=self.stride, padding=self.padding)
         # extract per-orientation filters into their own dim
         out = out.view(n, self.out_channels, 4, out.shape[-2], out.shape[-1])
         if self.bias is not None:
@@ -45,19 +45,19 @@ class ConvP4ToP4(nn.Module):
         self.stride = stride
         self.padding = padding
 
-        self._weight = nn.Parameter(torch.empty(out_channels, in_channels, 4, kernel_size, kernel_size).to(device))
-        nn.init.kaiming_uniform_(self._weight, a=5 ** 0.5)
+        self.weight = nn.Parameter(torch.empty(out_channels, in_channels, 4, kernel_size, kernel_size).to(device))
+        nn.init.kaiming_uniform_(self.weight, a=5 ** 0.5)
         self.bias = nn.Parameter(torch.zeros(out_channels).to(device)) if bias else None
 
     # inp: n x c_in x 4 x h x w
     def forward(self, inp):
         n = inp.shape[0]
         # make four copies of the filters, rotated element-wise AND around the feature map circle by 90deg each time
-        weight = torch.stack([self._weight.rot90(k, dims=(3, 4)).roll(k, 2) for k in range(4)], dim=1)
+        weight = torch.stack([self.weight.rot90(k, dims=(3, 4)).roll(k, 2) for k in range(4)], dim=1)
         # fold per-orientation filters into channel dims for convolution
         _inp = inp.view(n, self.in_channels * 4, inp.shape[-2], inp.shape[-1])
-        _weight = weight.view(self.out_channels * 4, self.in_channels * 4, self.kernel_size, self.kernel_size)
-        out = F.conv2d(_inp, _weight, stride=self.stride, padding=self.padding)
+        weight = weight.view(self.out_channels * 4, self.in_channels * 4, self.kernel_size, self.kernel_size)
+        out = F.conv2d(_inp, weight, stride=self.stride, padding=self.padding)
         # extract per-orientation filters into their own dim
         out = out.view(n, self.out_channels, 4, out.shape[-2], out.shape[-1])
         if self.bias is not None:
