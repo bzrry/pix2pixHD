fbbdac74ea2c19d629eb1aeb106683c2fd10a799
Tom Berry <tberry860@gmail.com>
2021-11-18 18:20:09 -0500

    stack filters at runtime

--- models/p4.py
+++ models/p4.py
@@ -22,15 +22,13 @@ class ConvZ2ToP4(nn.Module):
         nn.init.kaiming_uniform_(self._weight, a=5 ** 0.5)
         self.bias = nn.Parameter(torch.zeros(out_channels).to(device)) if bias else None
 
-        # make four copies of the filters, rotated element-wise
-        self.weight = torch.stack([self._weight.rot90(k, dims=(2, 3)) for k in range(4)], dim=1)
-        assert(self.weight.shape == (out_channels, 4, in_channels, kernel_size, kernel_size))
-
     # inp: n x c_in x h x w
     def forward(self, inp):
         n = inp.shape[0]
+        # make four copies of the filters, rotated element-wise
+        weight = torch.stack([self._weight.rot90(k, dims=(2, 3)) for k in range(4)], dim=1)
         # fold per-orientation filters into channel dims for convolution
-        _weight = self.weight.view(self.out_channels * 4, self.in_channels, self.kernel_size, self.kernel_size)
+        _weight = weight.view(self.out_channels * 4, self.in_channels, self.kernel_size, self.kernel_size)
         out = F.conv2d(inp, _weight, stride=self.stride, padding=self.padding)
         # extract per-orientation filters into their own dim
         out = out.view(n, self.out_channels, 4, out.shape[-2], out.shape[-1])
@@ -51,16 +49,14 @@ class ConvP4ToP4(nn.Module):
         nn.init.kaiming_uniform_(self._weight, a=5 ** 0.5)
         self.bias = nn.Parameter(torch.zeros(out_channels).to(device)) if bias else None
 
-        # make four copies of the filters, rotated element-wise AND around the feature map circle by 90deg each time
-        self.weight = torch.stack([self._weight.rot90(k, dims=(3, 4)).roll(k, 2) for k in range(4)], dim=1)
-        assert(self.weight.shape == (out_channels, 4, in_channels, 4, kernel_size, kernel_size))
-
     # inp: n x c_in x 4 x h x w
     def forward(self, inp):
         n = inp.shape[0]
+        # make four copies of the filters, rotated element-wise AND around the feature map circle by 90deg each time
+        weight = torch.stack([self._weight.rot90(k, dims=(3, 4)).roll(k, 2) for k in range(4)], dim=1)
         # fold per-orientation filters into channel dims for convolution
         _inp = inp.view(n, self.in_channels * 4, inp.shape[-2], inp.shape[-1])
-        _weight = self.weight.view(self.out_channels * 4, self.in_channels * 4, self.kernel_size, self.kernel_size)
+        _weight = weight.view(self.out_channels * 4, self.in_channels * 4, self.kernel_size, self.kernel_size)
         out = F.conv2d(_inp, _weight, stride=self.stride, padding=self.padding)
         # extract per-orientation filters into their own dim
         out = out.view(n, self.out_channels, 4, out.shape[-2], out.shape[-1])
